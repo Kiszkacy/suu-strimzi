5b24a8ff7c45f71b99fd180aeabdf9056f49fb31
ldydek <lukaszdydek72@gmail.com>
2025-05-28 12:18:51 +0200

    tests added

--- kafka-chat-frontend/src/App.test.js
+++ kafka-chat-frontend/src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders chat input fields and send button', () => {
+  render(<App/>);
+
+  const usernameInput = screen.getByPlaceholderText(/enter your name/i);
+  expect(usernameInput).toBeInTheDocument();
+
+  const messageInput = screen.getByPlaceholderText(/type your message/i);
+  expect(messageInput).toBeInTheDocument();
+
+  const sendButton = screen.getByRole('button', {name: /send/i});
+  expect(sendButton).toBeInTheDocument();
+});
+
+test('renders send button', () => {
+  render(<App/>);
+
+  const sendButton = screen.getByRole('button', {name: /send/i});
+  expect(sendButton).toBeInTheDocument();
 });
